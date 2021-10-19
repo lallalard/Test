8e2abd2316f73d4ec3ecad1d90623df809f2734a
lallalard <leoderheld9juliabox@gmail.com>
2021-10-19 13:26:25 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,8 +6,34 @@ export default function App() {
 }
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+      bgColor: 'white',
+    };
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button
+        className="square"
+        style={{ backgroundColor: this.state.bgColor }}
+        onClick={() =>
+          this.state.selected
+            ? this.setState({ bgColor: 'white', selected: false })
+            : this.setState({ bgColor: 'rgb(219, 219, 219)', selected: true })
+        }
+      >
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class InputSquare extends React.Component {
+  render() {
+    return <button className="square">{this.props.value}</button>;
   }
 }
 
@@ -42,12 +68,50 @@ class Board extends React.Component {
   }
 }
 
+class InputInterface extends React.Component {
+  renderSquare(i) {
+    return <InputSquare value={i} />;
+  }
+
+  render() {
+    const status = 'Eingabe';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+      </div>
+    );
+  }
+}
+
 class Game extends React.Component {
   render() {
     return (
       <div className="game">
         <div className="game-board">
           <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+        <div className="game-board">
+          <InputInterface />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
