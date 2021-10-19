05b84291075221fa5385f1cc8ac5b79afa401e3a
lallalard <leoderheld9juliabox@gmail.com>
2021-10-19 11:03:35 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class Board extends React.Component {
         <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
-          {this.renderSquare(6)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
           {this.renderSquare(6)}
